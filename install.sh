ff3c3cebe641d1c6a97f0628704800d0df4b66e7
Karl Boyle <karlboyle@gmail.com>
2018-02-17 21:24:43 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@
 dir="$HOME/workspace"
 mkdir -p $dir
 cd $dir
-git clone --recursive git://github.com/karlboyle/dotfiles.git
+git clone --recursive git@github.com:karlboyle/dotfiles.git
 cd dotfiles
 ./symlink-dotfiles.sh
